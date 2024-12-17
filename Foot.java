699ee3c506a99775883f93ee43dcf580c0cb2ee0
HotdogCATz <wutichaipatthaisong@gmail.com>
2024-12-17 20:21:54 +0700

    fix

--- Foot.java
+++ Foot.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author sarun
- */
-public abstract class Foot { //make this class abstract
-
-    public abstract String draw(); //make this method abstract
+public abstract class Foot { 
+    public abstract String draw();
 }
--- FootShape.java
+++ FootShape.java
@@ -1,19 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 import java.util.Scanner;
 
-/**
- *
- * @author sarun
- */
 public class FootShape {
 
-    /**
-     * @param args the command line arguments
-     */
     private Foot pencil;
 
     public void getDrawTypeFromUser(String drawType) {
